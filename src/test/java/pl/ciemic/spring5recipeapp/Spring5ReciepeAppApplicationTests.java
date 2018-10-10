f97d0eb5c048f9a92796afd51760bdb2b76dcd78
ciemic <michal.cieslar@gmail.com>
2018-10-10 15:36:25 +0200

    first commit

---
+++ src/test/java/pl/ciemic/spring5recipeapp/Spring5ReciepeAppApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.ciemic.spring5recipeapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5ReciepeAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
